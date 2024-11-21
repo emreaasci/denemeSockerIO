dd6f12844f9f242883609f255812d3076c882f41
emreasci <emreaasci@gmail.com>
2024-11-21 11:20:38 +0300

    Initial Commit

---
+++ denemeSockerIO/denemeSockerIOApp.swift
@@ -0,0 +1,17 @@
+//
+//  denemeSockerIOApp.swift
+//  denemeSockerIO
+//
+//  Created by Emre Aşcı on 21.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct denemeSockerIOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ denemeSockerIOTests/denemeSockerIOTests.swift
@@ -0,0 +1,17 @@
+//
+//  denemeSockerIOTests.swift
+//  denemeSockerIOTests
+//
+//  Created by Emre Aşcı on 21.11.2024.
+//
+
+import Testing
+@testable import denemeSockerIO
+
+struct denemeSockerIOTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
